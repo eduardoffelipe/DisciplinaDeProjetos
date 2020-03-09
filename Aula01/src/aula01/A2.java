21b1623c1a0d966f097c1c95dadebbd9120b8e79
eduardoffelipe <eduardoftoledo70@gmail.com>
2020-03-09 15:40:06 -0300

    first commit

---
+++ Aula01/src/aula01/A2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula01;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class A2 implements A {
+    public float taxa(){
+        return 1.8f;
+    }
+}
