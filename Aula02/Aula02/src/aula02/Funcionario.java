4e23d44db50a8565adf71d3689b4d4aed73894d5
eduardoffelipe <eduardoftoledo70@gmail.com>
2020-03-11 09:44:56 -0300

    AUla 02

---
+++ Aula02/Aula02/src/aula02/Funcionario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula02;
+
+/**
+ *
+ * @author eduardotoledo
+ */
+public  abstract class Funcionario {
+    protected String nome;
+    protected double salarioBase;
+    protected double salario;
+    
+}
